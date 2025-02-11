8e7b43f3747d148a921fd422fcc100f21e922f9a
esnoftz <esnoftz@gmail.com>
2025-02-11 07:24:34 -0600

    Initial Commit

---
+++ BasicFirebaseApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasicFirebaseApp
+//
+//  Created by EVANGELINE NOFTZ on 2/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BasicFirebaseAppTests/BasicFirebaseAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  BasicFirebaseAppTests.swift
+//  BasicFirebaseAppTests
+//
+//  Created by EVANGELINE NOFTZ on 2/11/25.
+//
+
+import Testing
+@testable import BasicFirebaseApp
+
+struct BasicFirebaseAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
